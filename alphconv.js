cc0a0261bd6883f09044e47585ccb59e357245bc
ShovelCode <rchss@rocketmail.com>
2020-07-02 16:11:11 -0500

    Add files via upload

---
+++ alphconv.js
@@ -0,0 +1,9 @@
+//js object
+var alph = [];
+alph["А"] = "A";
+alph["а"] = "a";
+alph["Ә"] = "Ä";
+alph["ә"] = "ä";
+var x = person.length;     // person.length will return 0
+var y = person[0];
+//still needs to be completed
