38698d3d44f8aeabc99fbbcd365d307617f97e02
ShovelCode <rchss@rocketmail.com>
2020-07-02 16:32:32 -0500

    Add files via upload

--- alphconv.js
+++ alphconv.js
@@ -4,6 +4,67 @@ alph["А"] = "A";
 alph["а"] = "a";
 alph["Ә"] = "Ä";
 alph["ә"] = "ä";
+alph["Б"] = "B";
+alph["б"] = "b";
+alph["В"] = "V";
+alph["в"] = "v";
+alph["Г"] = "G";
+alph["г"] = "g";
+alph["Ғ"] = "Ğ";
+alph["ғ"] = "ğ";
+alph["Д"] = "D";
+alph["д"] = "d";
+alph["Е"] = "E";
+alph["е"] = "e";
+alph["Ё"] = "Yo";
+alph["ё"] = "yo";
+alph["Ж"] = "Zh";
+alph["ж"] = "zh";
+alph["З"] = "Z";
+alph["з"] = "z";
+alph["И"] = "I";
+alph["и"] = "i";
+alph["Й"] = "Y";
+alph["й"] = "y";
+alph["К"] = "K";
+alph["к"] = "k";
+alph["Қ"] = "Q";
+alph["қ"] = "q";
+alph["Л"] = "L";
+alph["л"] = "l";
+alph["М"] = "M";
+alph["м"] = "m";
+alph["Н"] = "N";
+alph["н"] = "n";
+alph["Ң"] = "Ŋ";
+alph["ң"] = "ŋ";
+alph[""] = "";
+alph[""] = "";
+alph[""] = "";
 var x = person.length;     // person.length will return 0
 var y = person[0];
 //still needs to be completed
+//the opposite also needs to be made
+О O
+о o
+
+Ө Ö
+ө ö
+
+П P
+п p
+
+Р R
+р r
+
+С S
+с s
+
+Т T
+т t
+
+У U
+у u
+
+Ұ U
+ұ u
